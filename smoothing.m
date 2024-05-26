718c90bb4471c693f783640519e2461f4b8979c3
Vladislav <87985874+Stergrim@users.noreply.github.com>
2024-05-26 12:01:11 +0300

    Delete smoothing.m

--- smoothing.m
+++
@@ -1,8 +0,0 @@
-function smoothing(Storage)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-Storage.vectors_map = smoothdata2(Storage.vectors_map,"movmedian",4);
-Storage.vectors_map_last_pass = smoothdata2(Storage.vectors_map_last_pass,"movmedian",4);
-
-end
