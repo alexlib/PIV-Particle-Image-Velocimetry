40548a9dd45dde912080e51d2b93823ebbbcd9e8
Vladislav <87985874+Stergrim@users.noreply.github.com>
2024-06-12 13:05:13 +0300

    Update resize_field.m

--- matlab/resize_field.m
+++ matlab/resize_field.m
@@ -1,7 +1,7 @@
 function resize_field(Storage,size_map)
 %resize_field Масштабирование векторного поля
 %   Выполняет масштабирование векторного поля в соответствии с новыми
-%   размерами и наложение окон опроса
+%   размерами и наложением окон опроса
 
 % Метод масштабирования
 Storage.vectors_map = imresize(Storage.vectors_map,size_map,'bilinear');
